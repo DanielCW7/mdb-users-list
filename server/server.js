70c2acfe990949b11299ee6a317aeca990a18ae2
daniel <dev.danielcw@gmail.com>
2024-03-12 19:05:30 -0500

    BREAKING CHANGE(structure): revert back to old format

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const User = require("./models/userModel");
+
+// middleware 
+app.use(express.json())
+
+// database
+const connectDB = () => {
+    try {
+        mongoose.connect(uri)
+        console.log("connected to MDB")
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+connectDB()
+
+// routes
+
+app.get("/", (req, res) => {
+    res.send({"users": ["1", "2", "3"]})
+})
+
+app.post("/user", async (req, res) => {
+    try {
+        const user = await User.create(req.body)
+        res.status(200).json(user)
+    } catch(err) {
+        console.error(err)
+        res.send(500).json({ message: message.error })
+    }
+})
+
+app.listen(5000, () => {
+    console.log("Server running on 5000")
+})
